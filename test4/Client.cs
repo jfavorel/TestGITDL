a2a41b0bcb69ca0cf0e0045e49f9ae09c407cd3c
Jacques <jfavorel@gmail.com>
2018-04-11 14:58:00 +0200

    premier

---
+++ test4/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test4
+{
+    class Client
+    {
+        private string nom;
+        private string prenom;
+        private int numero;
+        
+        public Client(string Nom, string Prenom, int Numero)
+        {
+            nom = Nom;
+            prenom = Prenom;
+            numero = Numero;
+        }
+
+        public string Decrire()
+        {
+            string description = "Je m'appelle : " + nom + " " + prenom + " J'ai le numéro: " + numero;
+            return description;
+        }
+    }
+}
---
+++ test4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c1 = new Client("Favorel", "Jacques", 1280);
+            Console.WriteLine(c1.Decrire());
+            
+
+        }
+    }
+}
